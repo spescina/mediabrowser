776fe2bf0761e34672fbb5e431e9228faaddda5f
Simone Pescina <s.pescina@gmail.com>
2014-12-03 19:27:33 +0100

    no message

--- src/Spescina/Mediabrowser/Composers/MediabrowserComposer.php
+++ src/Spescina/Mediabrowser/Composers/MediabrowserComposer.php
@@ -16,11 +16,7 @@ class MediabrowserComposer {
         private function setupAssets()
         {
                 switch (App::environment()) {
-                        case 'staging':
-                        case 'production':
-                                break;
-
-                        case 'local':
+                        default:
                                 Asset::container('header')->add('bootstrap-css', 'packages/spescina/mediabrowser/src/css/vendor/bootstrap.css');
                                 Asset::container('header')->add('fontawesome-css', 'packages/spescina/mediabrowser/src/css/vendor/font-awesome.css');
                                 Asset::container('header')->add('uploader-css', 'packages/spescina/mediabrowser/src/css/vendor/jquery.fileupload.css');
