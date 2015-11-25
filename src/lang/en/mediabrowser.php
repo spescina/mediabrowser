d809dfb6ade54ff508a7ee0ccfbdea695ab9ff80
Simone Pescina <s.pescina@gmail.com>
2015-11-25 16:33:55 +0100

    added it translation and removed unused delete folder key

--- src/lang/en/mediabrowser.php
+++ src/lang/en/mediabrowser.php
@@ -3,9 +3,9 @@
 return array(
     'upload' => 'Upload',
     'create_folder' => 'New Folder',
-    'delete_folder' => 'Delete Folder',
     'select' => 'Select',
     'cancel' => 'Cancel',
-    'folder' => 'folder',
-    'confirm' => 'Create'
+    'folder' => 'folder name',
+    'confirm' => 'Create',
+    'confirm_delete' => 'Are you sure?'
 );
